e76f459c58ce24fddcb8de6b69bb5f55871a171e
= <=>
2025-12-02 08:21:03 +0100

    SPLICE

---
+++ 6.3splice.js
@@ -0,0 +1,6 @@
+let animals = ["gat", "ca", "peix", “cranc”];
+animals.splice(1, 2, "ocell"); // substitueix "ca" per "ocell"
+console.log(animals);
+animals.splice(2, 1, "ocell"); // substitueix "ca" per "ocell"
+console.log(animals);
+
