a1950e770ff534a375268118c87a27e95dfe23ed
Ivan7543 <icorderoromero@alumnat.iessacolomina.es>
2025-12-09 08:47:44 +0100

    9/12/25

---
+++ tuples7.1.js
@@ -0,0 +1,11 @@
+let fruita = {
+    0: "meló",
+    1: "síndria",
+    2: "albercoc",
+    length: 3
+};
+// Modificam
+fruita[1] = "préssec";
+fruita[3] = "papaia";
+fruita.length = 4;
+console.log(fruita);
