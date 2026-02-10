a80ce7f0fc24cff7247d9f74262a2199ac0eb480
Ivan7543 <icorderoromero@alumnat.iessacolomina.es>
2026-02-10 08:15:49 +0100

    10/02/2026

---
+++ 9.3.1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <title>Pràctica Chart.js</title>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</head>
+<body>
+    <h1>Exemple Chart.js</h1>
+    <canvas id="myChart" width="400" height="200"></canvas>
+    <script src="./main.js"></script>
+</body>
+</html>
